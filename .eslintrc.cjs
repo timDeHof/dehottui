e57b43b63d8c4026e4726c787ac1322b9b693c2d
timDeHof <this.is.tim.dehof@gmail.com>
2023-08-07 20:43:06 -0400

    chore(eslintrc.cjs): remove trailing comma and fix indentation feat(eslintrc.cjs): add support for latest ECMAScript version and module type chore(eslintrc.cjs): update eslint plugin versions chore(eslintrc.cjs): add storybook plugin to extends chore(eslintrc.cjs): add storybook plugin to plugins chore(eslintrc.cjs): remove unused rules chore(eslintrc.cjs): add missing newline at end of file feat(storybook/main.ts): add storybook configuration file feat(storybook/preview.ts): add storybook preview configuration file chore(package.json): add storybook dependencies chore(package.json): add storybook scripts chore(package.json): add storybook build script feat(assets/discord.svg): add Discord SVG icon
    feat: add github.svg and youtube.svg to assets folder
    
    feat: add Button.stories.tsx component file

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,63 +4,48 @@ module.exports = {
     "browser": true,
     "es2020": true,
     "node": true,
-    "vitest-globals/env": true,
+    "vitest-globals/env": true
   },
-  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module"
+  },
   ignorePatterns: ["dist", ".eslintrc.cjs", "node_modules", "coverage"],
-  extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:react/jsx-runtime",
-    "plugin:react-hooks/recommended",
-    "plugin:vitest/recommended",
-    "plugin:vitest-globals/recommended",
-  ],
-  overrides: [
-    {
-      files: [
-        "**/*.ts",
-        "**/*.tsx",
-        "**/*.js",
-        "**/*.jsx",
-        "**/__tests__/**/*.[jt]s?(x)",
-        "**/?(*.)+(spec|test).[jt]s?(x)",
-      ],
-      parser: "@typescript-eslint/parser",
-      settings: {
-        "react": { version: "detect" },
-        "import/resolver": {
-          typescript: {},
-        },
+  extends: ["eslint:recommended", "plugin:react/recommended", "plugin:react/jsx-runtime", "plugin:react-hooks/recommended", "plugin:vitest/recommended", "plugin:vitest-globals/recommended", "plugin:storybook/recommended"],
+  overrides: [{
+    files: ["**/*.ts", "**/*.tsx", "**/*.js", "**/*.jsx", "**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
+    parser: "@typescript-eslint/parser",
+    settings: {
+      "react": {
+        version: "detect"
       },
-      extends: ["plugin:testing-library/react"],
+      "import/resolver": {
+        typescript: {}
+      }
     },
-  ],
-  settings: { react: { version: "18.2" } },
+    extends: ["plugin:testing-library/react"]
+  }],
+  settings: {
+    react: {
+      version: "18.2"
+    }
+  },
   plugins: ["react-refresh", "vitest", "testing-library"],
   rules: {
-    "react-refresh/only-export-components": [
-      "warn",
-      { allowConstantExport: true },
-    ],
-    "no-restricted-imports": [
-      "error",
-      {
-        patterns: ["@/components/*/*"],
-      },
-    ],
+    "react-refresh/only-export-components": ["warn", {
+      allowConstantExport: true
+    }],
+    "no-restricted-imports": ["error", {
+      patterns: ["@/components/*/*"]
+    }],
     "import/default": "off",
     "import/no-named-as-default-member": "off",
     "import/no-named-as-default": "off",
-
     "react/react-in-jsx-scope": "off",
-    "vitest/max-nested-describe": [
-      "error",
-      {
-        max: 5,
-      },
-    ],
+    "vitest/max-nested-describe": ["error", {
+      max: 5
+    }],
     "prefer-rest-params": ["error"],
-    "no-unused-vars": "off",
-  },
+    "no-unused-vars": "off"
+  }
 };
